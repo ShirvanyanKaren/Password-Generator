e92c9d0e9000987c9f9dc0f7daa39863f899863e
Karen Shirvanyan <shivi@karens-mbp.attlocal.net>
2023-06-30 22:14:41 -0700

    Added shuffler and guaranteed value

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Password-Generator
+
+## Technology Used, Deployment, and Repository
+
+| Technology Used         | Resource URL           | 
+| ------------- |:-------------:| 
+| Javascript    | [https://developer.mozilla.org/en-US/docs/Web/HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) |   
+| Git | [https://git-scm.com/](https://git-scm.com/)     | 
+| Deployed Site | [https://shirvanyankaren.github.io/My-Professional-Portfolio/](https://shirvanyankaren.github.io/My-Professional-Portfolio/)     | 
+| My Github Repository | [https://github.com/ShirvanyanKaren/My-Professional-Portfolio](https://github.com/ShirvanyanKaren/My-Professional-Portfolio/)     | 
+
+## Description 
+This project was an on-the-job ticket that had preset code within the HTML and CSS for a password generator, with the main objective being to enable the javascript to perform the function of creating a randomized password
--- script.js
+++ script.js
@@ -24,103 +24,139 @@
 
 var generateBtn = document.querySelector("#generate");
 
-var lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-var upperChar = lowerChar.map(function(element) {
+var upperChar = lowerChar.map(function (element) {
   return element.toUpperCase();
 });
 console.log(upperChar)
 var numbersPass = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", , "]", "\\", "^", "_", "`", "{", "|", "}", "~", ]
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", , "]", "\\", "^", "_", "`", "{", "|", "}", "~",]
 
-var userOutput
-
-// blankPass var
-var blankPass
+var userOutput;
+// blankPass var to join password to
+var blankPass;
 // Based on choices, create availableChars variable to assign them to
-var availableChars
+var availableChars;
+var guaranteeChar;
 
 //Function called write password
-function writePassword(){
-// creates variable name "password" and sets it equal to return for function "generatePassword"
-var password = genPassword();
-// creates variable "passwordText" that grabs "id=password" in html
-var passwordText = document.querySelector("#password");
-// sets value to password
-passwordText.value = password;
+function writePassword() {
+  // creates variable name "password" and sets it equal to return for function "generatePassword"
+  var password = genPassword();
+  // creates variable "passwordText" that grabs "id=password" in html
+  var passwordText = document.querySelector("#password");
+  // sets value to password
+  passwordText.value = password;
 }
 generateBtn.addEventListener("click", writePassword);
 
 function genPassword() {
-  // Must prompt used for password length (8-128)
-  
-  
-var lengthPass = prompt("How many characters would you like your password to be? Choose between 8-128?");
 
-//Created a while loop if user doesn't meet sufficient requirements
-  while (lengthPass < 8 || lengthPass > 128 || !lengthPass){
-  alert("Number must be between 8 and 128");
-  lengthPass = prompt("Choose between 8-128");
+
+  // Must prompt used for password length (8-128) 
+  var lengthPass = Number(prompt("How many characters would you like your password to be? Choose between 8-128?"));
+  if (isNaN(lengthPass)){
+    alert("Input must be number");
+    return null;
+  }
+  //Created a while loop if user doesn't meet sufficient requirements
+  while (lengthPass < 8 || lengthPass > 128 || !lengthPass) {
+    alert("Number must be between 8 and 128");
+    return null;
   }
 
-// Confirm if user wants upper/lower, numbers and or special characters
-if (lengthPass => 8 && lengthPass <= 128) { 
-var confirmUpper = confirm("Would you like to use upper case characters?");
-var confirmLower = confirm("Would you like to use lower case characters?");
-var confirmNum = confirm("Would you like to use numbers?");
-var confirmSpecial = confirm("Would you like to use special characters?");
+  // Confirm if user wants upper/lower, numbers and or special characters
+  if (lengthPass => 8 && lengthPass <= 128) {
+    var confirmUpper = confirm("Would you like to use upper case characters?");
+    var confirmLower = confirm("Would you like to use lower case characters?");
+    var confirmNum = confirm("Would you like to use numbers?");
+    var confirmSpecial = confirm("Would you like to use special characters?");
 
-}
-// if no value is confirmed, prompt user to input at least one value
-while (!confirmNum && !confirmSpecial && !confirmLower && !confirmUpper) {
-alert("You have to select atleast one attribute.");
-var confirmUpper = confirm("Would you like to use upper case characters?");
-var confirmLower = confirm("Would you like to use upper case characters?");
-var confirmNum = confirm("Would you like to use numbers?");
-var confirmSpecial = confirm("Would you like to use special characters?");
-}
- 
-if (confirmLower) {
-  availableChars = lowerChar;
-}
- if (confirmUpper) {
-  availableChars = upperChar;
-}
- if (confirmNum) {
-  availableChars = numbersPass;
-}
-if (confirmSpecial) {
-  availableChars = specialChar;
-}
-if (confirmLower) {
-  availableChars = availableChars.concat(lowerChar);
-}
-if (confirmUpper) {
-  availableChars = availableChars.concat(upperChar);
-}
-if (confirmNum) {
-  availableChars = availableChars.concat(numbersPass);
-}
-if (confirmSpecial) {
-  availableChars = availableChars.concat(specialChar);
-}
+  }
+  // if no value is confirmed, prompt user to input at least one value
+  while (!confirmNum && !confirmSpecial && !confirmLower && !confirmUpper) {
+    alert("You have to select atleast one attribute.");
+    var confirmUpper = confirm("Would you like to use upper case characters?");
+    var confirmLower = confirm("Would you like to use upper case characters?");
+    var confirmNum = confirm("Would you like to use numbers?");
+    var confirmSpecial = confirm("Would you like to use special characters?");
+  }
+
+  var blankPass = [];
+
+//Created a randomizer for the confirms that ensures 
+  if (confirmLower) {
+    guaranteeChar = lowerChar[Math.floor(Math.random() * lowerChar.length)];
+    blankPass.push(guaranteeChar);
+  }
+  if (confirmUpper) {
+    guaranteeChar = upperChar[Math.floor(Math.random() * upperChar.length)];
+    blankPass.push(guaranteeChar);
+  }
+  if (confirmNum) {
+    guaranteeChar = numbersPass[Math.floor(Math.random() * numbersPass.length)];
+    blankPass.push(guaranteeChar);
+  }
+  if (confirmSpecial) {
+    guaranteeChar = specialChar[Math.floor(Math.random() * specialChar.length)];
+    blankPass.push(guaranteeChar);
+  }
+//these if statements are to ensure that if the user only chooses one option, the loop runs through it as availableChar
+  if (confirmLower) {
+      availableChars = lowerChar;
+    }
+    if (confirmUpper) {
+      availableChars = upperChar;
+    }
+    if (confirmNum) {
+      availableChars = numbersPass;
+    }
+    if (confirmSpecial) {
+      availableChars = specialChar;
+    }
+
+  if (confirmLower) {
+    availableChars = availableChars.concat(lowerChar);
+  }
+  if (confirmUpper) {
+    availableChars = availableChars.concat(upperChar);
+  }
+  if (confirmNum) {
+    availableChars = availableChars.concat(numbersPass);
+  }
+  if (confirmSpecial) {
+    availableChars = availableChars.concat(specialChar);
+  }
 
 
-var blankPass = []
+  
 
-for(var i = 0; i < lengthPass; i++) {
-userOutput = availableChars[Math.floor(Math.random() * availableChars.length)]
-blankPass.push(userOutput)
-console.log(userOutput);
+  // created for loop that generates password given the user choices, and continues until outcom is equal to the lengthPass stated by user
+  for (var i = blankPass.length; i < lengthPass; i++) {
+    userOutput = availableChars[Math.floor(Math.random() * availableChars.length)]
+    blankPass.push(userOutput)
+    console.log(userOutput);
+  }
+  //We push the blackPass var and join it to ("") so password can output the result as a string value which then becomes navigated to passwordText
+
+  //shuffle blanpass as your own challenge
+  var password = shuffle(blankPass);
+  return password.join('');
+  // console.log(password)
 }
-//We push the blackPass var and join it to ("") so password can output the result as a string value which then becomes navigated to passwordText
-var password = blankPass.join("");
-return password;
-// console.log(password)
-} 
 
 
 // Add event listener to generate button
 
-
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return a;
+}
